4dba969a138d1fba1e02abaf0cb7e3e9d165f3c8
Yago Nitto <yago_nitto@hotmail.com>
2025-11-18 17:25:55 -0300

    feat: understanding the scope of beans

--- src/main/java/com/example/architecture_spring/ManagedBean.java
+++ src/main/java/com/example/architecture_spring/ManagedBean.java
@@ -3,9 +3,17 @@ package com.example.architecture_spring;
 import com.example.architecture_spring.todos.TodoEntity;
 import com.example.architecture_spring.todos.TodoValidator;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope(BeanDefinition.SCOPE_SINGLETON) // default
+//@Scope(WebApplicationContext.SCOPE_APPLICATION)
+//@Scope(BeanDefinition.SCOPE_PROTOTYPE)
+//@Scope("request")
+//@Scope("session")
+//@Scope("application")
 public class ManagedBean {
 
     @Autowired
