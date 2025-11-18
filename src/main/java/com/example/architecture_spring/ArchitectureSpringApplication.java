6118ff5ea7f6c007ce972a895a3f10aed04f9a5b
Yago Nitto <yago_nitto@hotmail.com>
2025-11-18 14:12:16 -0300

    initial commit

---
+++ src/main/java/com/example/architecture_spring/ArchitectureSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.architecture_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchitectureSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArchitectureSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/architecture_spring/ArchitectureSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.architecture_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArchitectureSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
